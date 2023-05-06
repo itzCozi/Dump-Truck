feb7ff47e18aaa76c456d1953e2ffcd2bccf5976
Cooper <Cooperransom08@gmail.com>
2023-05-06 01:11:26 -0400

    add better handling

--- DumpTruck/DumpTruck.py
+++ DumpTruck/DumpTruck.py
@@ -1,6 +1,6 @@
 # TODO: Add a folder dump command where a file will be created
 # with the .dll's, .exe's, A license (if found), A readme(if found) and exe hex-dumps
-# TODO: Add more outputs to the console
+# TODO: Add completion and other outputs to the console
 # TODO: Create a help command
 # TODO: Work on readme.md
 # TODO: Add PID to getprocesses output
@@ -90,7 +90,7 @@ class commands:
     except Exception as e:
       print(f'ERROR: An unknown error was encountered. \n{e}\n')
       sys.exit(1)
-      
+
   def getProcesses():
     # Get all running processes
     try:
@@ -110,7 +110,7 @@ class commands:
           iterated.append(itemobj)
         else:
           output = output.replace(line, '')
-    
+
       for item in retlist:
         if item == None:
           retlist.remove(item)
@@ -153,55 +153,55 @@ class driver:
       except:
         pass
 
-    if arg1 == 'hexdump':
-      try:
-        commands.hexdump(arg2)
-      except Exception as e:
-        if not os.path.exists(arg2):
-          print(f'ERROR: Dumper cannot find {arg2} in file-system.')
+    try:
+      if arg1 == 'hexdump':
+        try:
+          commands.hexdump(arg2)
+        except Exception as e:
+          if not os.path.exists(arg2):
+            print(f'ERROR: Dumper cannot find {arg2} in file-system.')
+            sys.exit(0)
+          else:
+            print(f'ERROR: An unknown error was encountered. \n{e}\n')
+            sys.exit(1)
+      elif arg1 == 'dumplibs':
+        try:
+          commands.dumplibs()
           sys.exit(0)
-        else:
+        except Exception as e:
           print(f'ERROR: An unknown error was encountered. \n{e}\n')
           sys.exit(1)
-    elif arg1 == 'dumplibs':
-      try:
-        commands.dumplibs()
-        sys.exit(0)
-      except Exception as e:
-        print(f'ERROR: An unknown error was encountered. \n{e}\n')
-        sys.exit(1)
-    elif arg1 == 'get-processes':
-      try:
-        commands.getProcesses()
-        sys.exit(0)
-      except Exception as e:
-        print(f'ERROR: An unknown error was encountered. \n{e}\n')
-        sys.exit(1)
-    elif arg1 == 'kill-process':
-      try:
-        commands.killProcess(arg2)
-        sys.exit(0)
-      except Exception as e:
-        print(f'ERROR: A runtime error occurred, is the process running? \n{e}\n')
+      elif arg1 == 'get-processes':
+        try:
+          commands.getProcesses()
+          sys.exit(0)
+        except Exception as e:
+          print(f'ERROR: An unknown error was encountered. \n{e}\n')
+          sys.exit(1)
+      elif arg1 == 'kill-process':
+        try:
+          commands.killProcess(arg2)
+          sys.exit(0)
+        except Exception as e:
+          print(f'ERROR: A runtime error occurred, is the process running? \n{e}\n')
+          sys.exit(1)
+
+      else:
+        print('ERROR: The given argument is not recognized, try the help command.')
         sys.exit(1)
 
-    else:
-      print('ERROR: The given argument is not recognized, try the help command.')
+    except PermissionError:
+      print('ERROR: Action executed without required permissions.')
       sys.exit(1)
+    except UnboundLocalError:
+      print('ERROR: Please try again with all required parameters.')
+      sys.exit(1)
+    except Exception as e:
+      print(f'ERROR: An unknown error was encountered. \n{e}\n')
 
 
 if __name__ == '__main__':
-  try:
-    driver.argHandler()
-  except PermissionError:
-    print('ERROR: Action executed without required permissions.')
-    sys.exit(1)
-  except IndexError:
-    print('ERROR: No were arguments passed to dumper, check help command.')
-    sys.exit(1)
-  except Exception as e:
-    raise Exception(f'ERROR: An unknown error was encountered. \n{e}\n')
-
+  driver.argHandler()
 else:
   print(f'ERROR: You cannot import {__file__}.')
   sys.exit(1)
